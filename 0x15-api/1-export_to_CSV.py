d4850997d9587b8f95b43691ebfafa4f69889315
Hlangzti <Hlangzti@gmail.com>
2023-01-16 04:24:04 -0800

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-'''
-A script to export data in the CSV format.
-'''
+"""using the JSONPlaceholder API:
+   export information about the TODO list progress for a given employee ID
+   to a file (CSV format), file name must be: <user_id>.csv"""
 
 import csv
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    uid = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    user = requests.get(url, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        uid)
-    todo = requests.get(url, verify=False).json()
-    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for t in todo:
-            taskwriter.writerow([int(uid), user.get('username'),
-                                 t.get('completed'),
-                                 t.get('title')])
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+
+    with open(str(argv[1]) + ".csv", "w") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            writer.writerow([int(argv[1]), user.get('username'),
+                task.get('completed'), task.get('title')])
